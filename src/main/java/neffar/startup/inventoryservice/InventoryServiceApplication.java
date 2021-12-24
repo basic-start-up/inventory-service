e31856e1eb8fd6346fced4a97d5ac4f0765add4b
neffar <soufiane.neffar@gmail.com>
2021-12-25 00:38:08 +0100

    Operational API

--- src/main/java/neffar/startup/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/neffar/startup/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package neffar.startup.inventoryservice;
 
+import neffar.startup.inventoryservice.entity.Product;
+import neffar.startup.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,12 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository) {
+        return args -> {
+            productRepository.save(new Product(null, "HP ZBOOK", 950.99));
+            productRepository.save(new Product(null, "DELL XPS", 350.99));
+            productRepository.save(new Product(null, "MAC BOOK PRO", 999.99));
+        };
+    }
 }
---
+++ src/main/java/neffar/startup/inventoryservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package neffar.startup.inventoryservice.repository;
+
+import neffar.startup.inventoryservice.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
